baa83314dec68e1ef87fcffaccfe5401f863c79a
Tom Chinery <hello@tomchinery.com>
2015-06-08 20:00:53 +0100

    Added correct scopes to Omniauth initialiser

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '747106637186-qa791ta31636rfss080h752h4cb4ecna.apps.googleusercontent.com', 'vw-9FfBYpwMvc_xg0CYLk4lf',
     {
-      client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}
+      client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}},
+      scope: "contacts, email"
     }
 end
