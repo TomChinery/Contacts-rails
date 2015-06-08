05a7aaa3d53fed0c81bd92f924998aba011f1317
Tom Chinery <hello@tomchinery.com>
2015-06-08 19:57:31 +0100

    Only get all contacts if there is a current_user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def index
 
-    contacts = Contact.all!(current_user.oauth_token)
+    contacts = Contact.all!(current_user.oauth_token, current_user.email) if current_user
 
   end
 end
