3ee0ade3bcf936535f845921677962ebca98ddef
Tom Chinery <hello@tomchinery.com>
2015-06-08 19:13:40 +0100

    Added create to Session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,20 @@
 class SessionsController < ApplicationController
   def create
+    user = User.create_from_omniauth(user_params)
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def delete
+    
+
   end
+
+  private
+
+  def user_params
+    params[:auth] = env["omniauth.auth"]
+    params.require(:auth).permit!
+  end
+
 end
