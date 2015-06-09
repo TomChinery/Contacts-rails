c00beb25ff5e088959f7e02112168a5ee83f840b
Tom Chinery <hello@tomchinery.com>
2015-06-09 12:23:57 +0100

    Successfully collecting contact data in model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,17 +8,33 @@ class Contact < ActiveRecord::Base
 
     headers = { "Authorization" => "Bearer " + token, "GData-Version" => "3.0" }
 
-    response = HTTParty.get("https://www.google.com/m8/feeds/contacts/#{email}/full", :headers => headers)
+    response = HTTParty.get("https://www.google.com/m8/feeds/contacts/#{email}/full?max-results=100", :headers => headers)
+
+    gmail_xml = Nokogiri::XML(response)
+
+    contacts = []
+
+    gmail_xml.css("entry").each do |entry|
+
+      name = entry.css("gd|fullName").text
+
+      email = !entry.css('gd|email').empty? ? entry.css('gd|email').attribute('address').text : ""
+
+      phone = entry.css("gd|phoneNumber").text
+
+      contact = {
+        :name => name,
+        :email => email,
+        :phone => phone,
+      }
+
+      contacts << contact
+
+    end
 
     binding.pry
-  end
-
-  def xml_to_object(xml_string)
-
-    object = OpenStruct.new( Hash.from_xml(xml_string) )
-
-    return object
 
   end
+
 
 end
